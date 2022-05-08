5393ebb6da3ecd3fd1d25af3dde60a0abbca4a7b
vladpavlix771 <56918679+vladpavlix771@users.noreply.github.com>
2022-05-08 23:22:47 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+#Токен вашего бота
+bot_token = '5361212615:AAHHUJiiUyXc51i2XmsSnQVakuRpEFkNRQc'
+#ID админа
+adminn = 279585801
+admin = 1370967532
+#Юзер саппорта
+support = '@blackToT_tg'
+#Канал с отзывами
+feedback = 'https://t.me/Black_ToT_shop'
+#Название магазина
+shop_name = 'Black_ToT'
---
+++ config.py.bak
@@ -0,0 +1,11 @@
+#Токен вашего бота
+bot_token = '5323110502:AAEfmpiyQwZknjHmFtyQQoKGdo7R7epfR8s'
+#ID админа
+adminn = 279585801
+admin = 1370967532
+#Юзер саппорта
+support = '@blackToT_tg'
+#Канал с отзывами
+feedback = 'https://t.me/Black_ToT_shop'
+#Название магазина
+shop_name = 'Black_ToT'
